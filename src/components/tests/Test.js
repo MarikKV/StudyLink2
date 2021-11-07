4af43851e4434fbb0aad8081002e0732202ec46a
kosaniakm <kosaniakm@gmail.com>
2021-11-07 23:00:19 +0200

    sfd

--- src/components/tests/Test.js
+++ src/components/tests/Test.js
@@ -10,7 +10,6 @@ export default function Test(props) {
     const status = useSelector(state => state.status);
 
     const [line, setLine] = useState("100%");
-    const [left, setLeft] = useState(0);
 
     const [lineStyles, setLineStyles] = useState({
         "height": "10px",
@@ -19,58 +18,51 @@ export default function Test(props) {
 
     useEffect(()=>{
         if(props.test.time != null){
-            setLeft(props.test.time)
-            let start = props.test.time;
-            let left = props.test.time;
-            let period = 100/start;
-  
-            const timer = setInterval(()=>{
-                left--;
-                setLeft(left => left - 1)
-                if(left <= 0){
-                    console.log("Time out!");
-                    clearInterval(timer)
-                }
-                setLine(line => {
-                    if(left < start*(0.65) && left > start*0.35){
-                        setLineStyles(style => { 
-                            return {
-                                ...style,
-                                "background": "orange" 
-                            }
-                        })
-                    }else if(left <= start*(0.35)){
-                        setLineStyles(style => { 
-                            return {
-                                ...style,
-                                "background": "red" 
-                            }
-                        })
-                    }
-                    let newLineVal = Number(line.replace("%","") - period);
-                    if(newLineVal <= 1) {
-                        document.getElementById('line').classList.remove('test_line');
-                        setLineStyles(style => { 
-                            return {
-                                ...style,
-                                "background": "green" 
-                            }
-                        })
-                        setTimeout( ()=>{document.getElementById('line').classList.add('test_line')}, 500)
-                        return "100%"; 
-                    }
-                    return Number(line.replace("%","") - period) + "%"
-                } )
-            }, 1000)
 
-            return () => clearInterval(timer);
+            let start = props.test.time;
+            let left = props.timeLeft;
+            setLine(line => {
+               
+                if(left < start*(0.65) && left > start*0.35){
+                    setLineStyles(style => { 
+                        return {
+                            ...style,
+                            "background": "orange" 
+                        }
+                    })
+                }
+                
+                else if(left <= start*(0.35) && start>=1 && left >=1){
+                    setLineStyles(style => { 
+                        return {
+                            ...style,
+                            "background": "red" 
+                        }
+                    })
+                }
+                
+                let newLineVal = (100*left)/start;
+                
+                if(newLineVal < start/100) {
+                    document.getElementById('line').classList.remove('test_line');
+                    setLineStyles(style => { 
+                        return {
+                            ...style,
+                            "background": "green" 
+                        }
+                    })
+                    setTimeout( ()=>{document.getElementById('line').classList.add('test_line')}, 1000 )
+                    return "100%"; 
+                }
+                return newLineVal + "%"
+            } )
         }
     },[status, props])
 
     return (
         
         <div className="mt-5">
-            <h4>Часу залишилось: {left}</h4>
+            <h4>Часу залишилось: {props.timeLeft}</h4>
             <hr style={lineStyles} id="line" className="test_line" align="left" color="green" width={line} />
             
             <h2 className="text-left mt-5">{props.test.question}</h2>
--- src/components/tests/Tests.js
+++ src/components/tests/Tests.js
@@ -12,6 +12,7 @@ export default function Tests() {
     const [tests, setTests] = useState([]);
     const [test, setTest] = useState([]);
     const [testIndex, setTestIndex] = useState([]);
+    const [timeLeft, setTimeLeft] = useState(0);
 
     const loadNewTest = function(test, testIndex){
         setTest(test);
@@ -33,6 +34,7 @@ export default function Tests() {
                 let timeForTest = data[0].time;
                 let i=0;
                 loadNewTest(data[i], i)
+                setTimeLeft(data[i].time)
                 timer = setInterval(()=>{
                     timeForTest--;
                     if(timeForTest<=0){
@@ -40,7 +42,8 @@ export default function Tests() {
                         loadNewTest(data[i], i);
                         timeForTest = data[i].time;
                     }
-                    if(i >= data.length){clearInterval(timer)}
+                    setTimeLeft(timeForTest)
+                    if(i >= data.length - 1){clearInterval(timer)}
                 }, 1000)
             })
             .catch( error => console.log(error) );
@@ -53,7 +56,7 @@ export default function Tests() {
             {isLogged ? 
             <>
                 <h1 className="text-center mt-3">Тести по пройденим темам</h1>
-                <Test test={test} testIndex={testIndex}/>
+                <Test test={test} testIndex={testIndex} timeLeft={timeLeft}/>
             </>
             :
             <h1 className="text-center mt-3">Увійдіть щоб проходити тести</h1>
