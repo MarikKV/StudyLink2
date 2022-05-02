426dc43bb06ee8c773a2c61ace63839205aaf5db
MarikTarta <marian@tarta.ai>
2022-05-02 14:39:48 +0300

    add kursach link

--- src/components/Kursova.js
+++ src/components/Kursova.js
@@ -8,6 +8,7 @@ import { Form, Button } from 'react-bootstrap';
 export default function Kursova() {
     const isLogged = useSelector(state => state.isLogged);
     const user = useSelector(state => state.saveStudentInfo);
+    const [userInfo, updateUserInfo] = useState({});
     const kursach_1 = useRef("");
     const kursach_2 = useRef("");
     const kursach_3 = useRef("");
@@ -16,20 +17,19 @@ export default function Kursova() {
     
     const updateUserKursach = async function(){
         const userInfo = (await db.collection("Students").doc(user.id).get()).data();
-        console.log(userInfo)
-        kursach_1.current = userInfo.kursach_1;
-        kursach_2.current = userInfo.kursach_2;
-        kursach_3.current = userInfo.kursach_3;
+        
+        if(kursach_1.current){userInfo.kursach_1 = kursach_1.current.value}
+        if(kursach_2.current){userInfo.kursach_2 = kursach_2.current.value}
+        if(kursach_3.current){userInfo.kursach_3 = kursach_3.current.value}
+        const res = await db.collection("Students").doc(user.id).update(userInfo);
+        console.log(res)
     }
     const getOldLinkValues = async function(){
         const userInfo = (await db.collection("Students").doc(user.id).get()).data();
-        kursach_1.current.value = userInfo.kursach_1 != undefined ? userInfo.kursach_1 : ""; 
-        if(kursach_2.current){
-            kursach_2.current.value = userInfo.kursach_2 != undefined ? userInfo.kursach_2 : ""; 
-        }
-        if(kursach_3.current){
-           kursach_3.current.value = userInfo.kursach_3 != undefined ? userInfo.kursach_3 : ""; 
-        }
+        if(kursach_1.current){kursach_1.current.value = userInfo.kursach_1 != undefined ? userInfo.kursach_1 : ""}
+        if(kursach_2.current){kursach_2.current.value = userInfo.kursach_2 != undefined ? userInfo.kursach_2 : ""}
+        if(kursach_3.current){kursach_3.current.value = userInfo.kursach_3 != undefined ? userInfo.kursach_3 : ""}
+        updateUserInfo(userInfo)
     }
     useEffect(() => {
         getOldLinkValues();
@@ -49,18 +49,17 @@ export default function Kursova() {
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Курсова робота №1 (HTML/CSS)</Form.Label>
                                 <div className="d-flex align-items-center">
-                                    <Form.Control type="email" placeholder="Link on github" ref={kursach_1} />
-                                    <a className='link-primary ml-3' target="_blank" href="#">show</a>
+                                    <Form.Control type="text" placeholder="Link on github" ref={kursach_1} />
+                                    <a className='link-primary ml-3' target="_blank" href={userInfo.kursach_1}>Відкрити</a>
                                 </div>
                             </Form.Group>
-
                             {
                                 user.kurs > 1 ?
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label>Курсова робота №1 (HTML/CSS)</Form.Label>
+                                    <Form.Label>Курсова робота №2 (HTML/CSS/JS)</Form.Label>
                                     <div className="d-flex align-items-center">
-                                        <Form.Control type="email" placeholder="Link on github" ref={kursach_2} />
-                                        <a className='link-primary ml-3' target="_blank" href="#">show</a>
+                                        <Form.Control type="text" placeholder="Link on github" ref={kursach_2} />
+                                        <a className='link-primary ml-3' target="_blank" href={kursach_2}>Відкрити</a>
                                     </div>
                                 </Form.Group>
                                 : null
@@ -69,20 +68,20 @@ export default function Kursova() {
                             {
                                 user.kurs > 2 ?
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label>Курсова робота №1 (HTML/CSS)</Form.Label>
+                                    <Form.Label>Курсова робота №3 (HTML/CSS/JS)</Form.Label>
                                     <div className="d-flex align-items-center">
-                                        <Form.Control type="email" placeholder="Link on github" ref={kursach_2} />
-                                        <a className='link-primary ml-3' target="_blank" href="#">show</a>
+                                        <Form.Control type="text" placeholder="Link on github" ref={kursach_3} />
+                                        <a className='link-primary ml-3' target="_blank" href={kursach_3}>Відкрити</a>
                                     </div>
                                 </Form.Group>
                                 : null
                             }
-
-                            <Button type="button" onClick={() => updateUserKursach()}>Зберегти</Button>
+                            <div align="center">
+                                <Button type="button" onClick={() => updateUserKursach()}>Зберегти</Button>
+                            </div>
                         </Form>
                     </div>
                 </>
-            
             }
         </div>
     )
