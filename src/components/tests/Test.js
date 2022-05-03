35db54372e883c93f0cf3dfc0a7babc5b8faace1
MarikTarta <marian@tarta.ai>
2022-05-03 17:45:06 +0300

    add test

--- src/components/tests/Test.js
+++ src/components/tests/Test.js
@@ -4,12 +4,13 @@ import { db } from '../../firebase';
 import '../../styles/Test.scss';
 
 export default function Test(props) {
-
     const isLogged = useSelector(state => state.isLogged);
     const user = useSelector(state => state.saveStudentInfo);
     const status = useSelector(state => state.status);
     const time_multilpier = 2;
 
+    const [timeStart, setTimeStart] = useState(10);
+    const [timeLeft, setTimeLeft, timeRef] = useState(10);
     const [line, setLine] = useState("100%");
 
     const [lineStyles, setLineStyles] = useState({
@@ -18,44 +19,52 @@ export default function Test(props) {
     });
 
     const chuseAnswer = function(element, unsver){
-        cleareAnswers();
+        props.cleareAnswers();
         element.classList.add('selected-answer');
-        console.log(unsver)
-    }
-
-    const cleareAnswers = function(){
-        document.querySelectorAll('.unsver').forEach(e=>e.classList.remove('selected-answer'));
-        console.log('clear')
+        props.submitUnsver(unsver);
     }
 
     useEffect(()=>{
-        if(props.test.time != null){
-
-            let start = props.test.time*time_multilpier;
-            let left = props.timeLeft;
-            setLine(line => {
-               
-                if(left >= start*(0.65)){
-                    setLineStyles(style => { return { ...style, "background": "green"  } })
-                }
-                else if(left < start*(0.65) && left > start*0.35){
-                    setLineStyles(style => { return { ...style, "background": "orange" } })
-                }
-                else if(left <= start*(0.35) && start>=1 && left >=1){
-                    setLineStyles(style => { return { ...style, "background": "red"    } })
-                }
-                
-                return (100*left)/start + "%";
-            } )
+        //cleareAnswers();
+        if(props.test.time > 0)
+        {
+            setTimeLeft(props.test.time * time_multilpier);
+            setTimeStart(props.test.time * time_multilpier);
+            /*
+            const updateLine = function(){
+                setLine(line => {
+                    let timeLeft;
+                    setTimeLeft(currentState  => {
+                        timeLeft = currentState  - 1;
+                        if(timeLeft <=0 )clearInterval(timer)
+                        return timeLeft;
+                    });
+                    if(timeLeft >= timeStart*(0.65)){
+                        setLineStyles(style => { return { ...style, "background": "green"  } })
+                    }
+                    else if(timeLeft < timeStart*(0.65) && timeLeft > timeStart*0.35){
+                        setLineStyles(style => { return { ...style, "background": "orange" } })
+                    }
+                    else if(timeLeft <= timeStart*(0.35) && timeStart >= 1 && timeLeft >= 1){
+                        setLineStyles(style => { return { ...style, "background": "red"    } })
+                    }
+                    console.log(timeLeft);
+                    console.log(timeStart)
+                    return (100*timeLeft)/timeStart + "%";
+                })
+            } 
+            let timer = setInterval(updateLine, 1000);
+            return () => clearInterval(timer);
+            */
         }
-    }, [status, props])
+    }, [props])
 
     return (
         <div className="mt-5">
-            { props.timeLeft == 1 ? cleareAnswers() : null }
-            <h4>Часу залишилось: {props.timeLeft}</h4>
+            {/*
+            <h4>Часу залишилось: {timeLeft}</h4>
             <hr style={lineStyles} id="line" className="test_line" align="left" color="green" width={line} />
-            
+            */}
             <h2 className="text-left mt-5">{props.test.question}</h2>
            
             <div className="my-5">
--- src/components/tests/Tests.js
+++ src/components/tests/Tests.js
@@ -12,35 +12,60 @@ export default function Tests() {
 
     const [tests, setTests] = useState([]);
     const [test, setTest] = useState([]);
-    const [testIndex, setTestIndex] = useState([]);
-    const [timeLeft, setTimeLeft] = useState(0);
+    const [testActive, setTestActive] = useState(0);
+    const [userUnsvers, setUserUnsvers] = useState([]);
+    const [testStats, setTestStats] = useState({});
 
-    const loadNewTest = function(test, testIndex){
-        setTest(test);
-        setTestIndex(testIndex)
-    }
     const getTests = async function(){
-        let timer;
         let tests = [];
         (await db.collection("Tests").get()).forEach(test => tests.push({id: test.id, ...test.data()}))
         setTests(tests);
-            let timeForTest = tests[0].time * time_multilpier;
-            let i = 0;
-            loadNewTest(tests[i], i)
-            setTimeLeft(tests[i].time * time_multilpier)
-            timer = setInterval(()=>{
-                timeForTest--;
-                if(timeForTest <= 0){
-                    i++;
-                    loadNewTest(tests[i], i);
-                    timeForTest = tests[i].time * time_multilpier;
-                }
-                setTimeLeft(timeForTest)
-                if(i >= tests.length - 1){ clearInterval(timer) }
-            }, 1000
-        )
-        return () => clearInterval(timer);
+        setTestActive(0);
+        setTest(tests[0]);
     }
+
+    const loadNextTest = function(){
+        let newTestN = testActive + 1;
+        if(newTestN >= tests.length) {
+            if(newTestN <= tests.length) setTestActive(newTestN);
+            countPoints();
+            return
+        };
+        
+        setTestActive(newTestN);
+        setTest(tests[newTestN]);
+        cleareAnswers();
+    }
+
+    const submitUnsver = function(unsver){
+        let newUserUnsver = tests[testActive];
+        newUserUnsver.userUnsver = unsver;
+        newUserUnsver.unsverRight == newUserUnsver.userUnsver ? newUserUnsver.points = 1 : newUserUnsver.points = 0;
+        setUserUnsvers(unsver => { 
+            if(unsver.filter(t => t.id == newUserUnsver.id)?.length == 0){
+                return [...unsver, newUserUnsver];
+            }else{
+                return [...unsver.filter(t => t.id != newUserUnsver.id), newUserUnsver];
+            }
+        });
+        console.log(userUnsvers);
+    }
+
+    const countPoints = function(){
+        let totalPoints = 0;
+        let unsversRight = 0;
+        let unsversFalse = 0;
+        userUnsvers.forEach(u => {
+            totalPoints += u.points; 
+            u.points > 0 ? unsversRight++ : unsversFalse++; 
+        })
+        setTestStats(stats => {return { totalPoints, unsversRight, unsversFalse }})
+    }
+
+    const cleareAnswers = function(){
+        document.querySelectorAll('.unsver').forEach(e=>e.classList.remove('selected-answer'));
+    }
+
     useEffect(()=>{
         if(isLogged){
             getTests();
@@ -52,10 +77,24 @@ export default function Tests() {
             {isLogged ? 
             <>
                 <h1 className="text-center mt-3">Тести по пройденим темам</h1>
-                <Test test={test} testIndex={testIndex} timeLeft={timeLeft}/>
-                <div align="right">
-                    <button className="btn btn-primary">Наступний</button>
-                </div>
+                {
+                    testActive == tests.length && tests.length != 0 ? 
+                    <div className='mt-5'>
+                        <h1>Тест завершено!</h1> 
+                        <div>
+                            <p className='text-left'>Набрано балів: {testStats.totalPoints}</p>
+                            <p className='text-left'>Правильних відповідей: {testStats.unsversRight}</p>
+                            <p className='text-left'>Невірних відповідей: {testStats.unsversFalse}</p>
+                        </div>
+                    </div>
+                    :
+                    <div>
+                        <Test test={test} submitUnsver={submitUnsver} cleareAnswers={cleareAnswers}/>
+                        <div align="right">
+                            <button className="btn btn-primary" onClick={loadNextTest}>Наступний</button>
+                        </div>
+                    </div>
+                }
             </>
             :
             <h1 className="text-center mt-3">Увійдіть щоб проходити тести</h1>
